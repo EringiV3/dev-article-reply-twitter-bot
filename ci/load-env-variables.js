5c7b4b3c5dbf8601704b9adcbe08ae6ce587fc72
萩原孝大 <kodaihagihara@EringiV3.local>
2021-01-17 16:06:57 +0900

    add deploy step

--- ci/load-env-variables.js
+++ ci/load-env-variables.js
@@ -1,2 +1,11 @@
-console.log("hoge");
-console.log(process.env);
+const fs = require("fs");
+
+const template = readFileSync("app.template.yaml").toString();
+let content = template;
+
+for (const match of template.matchAll(/\${{([A-Z_]*)}}/g)) {
+  const envName = `APP_${match[1]}`;
+  content = content.replace(match[0], `'${process.env[envName]}'`);
+}
+
+fs.writeFileSync("app.yaml", content);
